079fa04176ab735c8bc35271945c1058f6d76c3b
quartic <frank@quartic.ai>
2022-03-22 21:09:48 -0400

    Return more sample info

--- data.py
+++ data.py
@@ -16,9 +16,11 @@ class DAGMDataset(Dataset):
         img_path = self.meta_df["img_file"].iloc[idx]
         image = read_image(img_path)
         label = self.meta_df["class"].iloc[idx]
+        has_defect = self.meta_df["has_defect"].iloc[idx] == 1
+        defect_mask_path = str(self.meta_df["label_file"].iloc[idx])
 
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
-        return image, label
+        return image, label, img_path, has_defect, defect_mask_path
--- main.py
+++ main.py
@@ -10,21 +10,29 @@ import matplotlib.pyplot as plt
 def run():
     num_classes = 10
     ohe = Lambda(lambda y: torch.zeros(num_classes, dtype=torch.float).scatter_(dim=0, index=torch.tensor(y), value=1))
+
     training_set = DAGMDataset(meta_file=TRAINING_LABEL_FILE_PATH, target_transform=ohe)
     testing_set = DAGMDataset(meta_file=TEST_LABEL_FILE_PATH, target_transform=ohe)
+
     train_dataloader = DataLoader(training_set, batch_size=64, shuffle=True)
     test_dataloader = DataLoader(testing_set, batch_size=64, shuffle=True)
 
-    train_features, train_labels = next(iter(train_dataloader))
-    print(type(train_features))
-    print(type(train_labels))
+    train_features, train_labels, img_paths, has_defects, defect_mask_paths = next(iter(train_dataloader))
+
     print(f"Feature batch shape: {train_features.size()}")
     print(f"Labels batch shape: {train_labels.size()}")
     img = train_features[0].squeeze()
     label = train_labels[0]
+    image_path = img_paths[0]
+    has_defect = has_defects[0]
+    defect_mask_path = defect_mask_paths[0]
     plt.imshow(img, cmap="gray")
     plt.show()
+
     print(f"Label: {label}")
+    print(f"Image path: {image_path}")
+    print(f"Has defect: {has_defect}")
+    print(f"Defect image path: {defect_mask_path}")
 
 
 if __name__ == '__main__':
